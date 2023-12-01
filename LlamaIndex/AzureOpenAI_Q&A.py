5bc2c4061e46cb54413c61f96716ad86df128455
jeanaristide <jeanaristide34@gmail.com>
2023-12-01 11:28:03 +0800

    added essay and py file

---
+++ LlamaIndex/AzureOpenAI_Q&A.py
@@ -0,0 +1,59 @@
+import os
+from llama_index.llms import AzureOpenAI
+from llama_index.embeddings import AzureOpenAIEmbedding
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+import logging
+import sys
+from dotenv import load_dotenv
+load_dotenv('/Users/jeana/RAG/RAG/LlamaIndex/.env')
+
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO
+)  # logging.DEBUG for more verbose output
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+api_key = os.environ['OPENAI_API_KEY']
+azure_endpoint = os.environ['OPENAI_DEPLOYMENT_ENDPOINT']
+api_version = os.environ['OPENAI_DEPLOYMENT_VERSION']
+
+
+llm = AzureOpenAI(
+    model= os.environ['OPENAI_MODEL_NAME'],
+    deployment_name= os.environ['OPENAI_DEPLOYMENT_NAME'],
+    api_key=api_key,
+    azure_endpoint=azure_endpoint,
+    api_version=api_version,
+)
+
+# # You need to deploy your own embedding model as well as your own chat completion model
+embed_model = AzureOpenAIEmbedding(
+    model=os.environ['OPENAI_ADA_EMBEDDING_MODEL_NAME'],
+    deployment_name=os.environ['OPENAI_ADA_EMBEDDING_DEPLOYMENT_NAME'],
+    api_key=api_key,
+    azure_endpoint=azure_endpoint,
+    api_version=api_version,
+)
+
+from llama_index import set_global_service_context
+
+service_context = ServiceContext.from_defaults(
+    llm=llm,
+    embed_model=embed_model,
+)
+
+set_global_service_context(service_context)
+
+documents = SimpleDirectoryReader(
+    input_files=[r"/Users/jeana/RAG/RAG/LlamaIndex/paul_graham_essay.txt"] #or just indicate the fullpath of the folder containing the data
+).load_data()
+index = VectorStoreIndex.from_documents(documents)
+
+while True:
+    query = input("User: ")
+    if query == "exit":
+        break
+    query_engine = index.as_query_engine()
+    answer = query_engine.query(query)
+    print(answer.get_formatted_sources())
+    print("query was:", query)
+    print("answer was:", answer)
