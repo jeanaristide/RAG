004cf1745dc2d13dc38309c63941fd486cf2066c
jeanaristide <jeanaristide34@gmail.com>
2023-12-27 15:23:42 +1100

    modified:   LlamaIndex/AzureOpenAI_naive.ipynb 	modified:   LlamaIndex/RAG_TruLens_eval.ipynb 	modified:   LlamaIndex/default.sqlite 	new file:   LlamaIndex/storage/text-embedding-ada-002/default__vector_store.json 	new file:   LlamaIndex/storage/text-embedding-ada-002/docstore.json 	new file:   LlamaIndex/storage/text-embedding-ada-002/graph_store.json 	new file:   LlamaIndex/storage/text-embedding-ada-002/image__vector_store.json 	new file:   LlamaIndex/storage/text-embedding-ada-002/index_store.json 	modified:   Streamlit/app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,85 +1,147 @@
 import os
-import streamlit as st
-from llama_index.indices.list import SummaryIndex
-from llama_index import (
-    Document,
-    ServiceContext,
-    load_index_from_storage,
-)
-from llama_index.llms import OpenAI
+import pandas as pd
+from llama_index.llms import AzureOpenAI
+from llama_index.embeddings import AzureOpenAIEmbedding
+from langchain.embeddings import HuggingFaceEmbeddings
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, StorageContext, load_index_from_storage, set_global_service_context
+import logging
+import sys
+from dotenv import load_dotenv
+from llama_index.llms import Ollama
+import numpy as np
+from trulens_eval import TruLlama, Feedback, Tru, feedback
+from langchain.llms import Ollama
+from trulens_eval.tru_custom_app import instrument
+from trulens_eval import LiteLLM
+import litellm
+litellm.set_verbose=False
+tru = Tru()
 
 
-def get_llm(llm_name, model_temperature, api_key, max_tokens=256):
-    os.environ["OPENAI_API_KEY"] = api_key
-    return OpenAI(
-        temperature=model_temperature, model=llm_name, max_tokens=max_tokens
-    )
+class modelException(Exception):
+    def __init__(self, invalid_value, allowed_values):
+        self.invalid_value = invalid_value
+        self.allowed_values = allowed_values
+        message = f"Invalid value: {invalid_value}. Allowed values are: {', '.join(allowed_values)}"
+        super().__init__(message)
 
+def feedbacks(llm_name):
 
-def extract_terms(
-    documents, term_extract_str, llm_name, model_temperature, api_key
-):
-    llm = get_llm(llm_name, model_temperature, api_key, max_tokens=1024)
+    ##### INITIALZE FEEDBACK FUNCTION(S)#######
+    if llm_name == 'azureoai':
+        # Initialize AzureOpenAI-based feedback function collection class:
+        llm_provider = feedback.AzureOpenAI(
+                                        deployment_name=os.environ['OPENAI_DEPLOYMENT_NAME'],
+                                        api_key = os.environ['OPENAI_API_KEY'],
+                                        api_version=os.environ['OPENAI_DEPLOYMENT_VERSION'],
+                                        azure_endpoint=os.environ['OPENAI_DEPLOYMENT_ENDPOINT'],
+                                        # model = os.environ['OPENAI_MODEL_NAME']
+                                        )
+    elif llm_name == 'llama2':
+        llm_provider = LiteLLM(model_engine="ollama/llama2", api_base='http://localhost:11434')
 
-    service_context = ServiceContext.from_defaults(llm=llm, chunk_size=1024)
+    # Question/answer relevance between overall question and answer.
+    f_qa_relevance = Feedback(llm_provider.relevance, name = "Answer Relevance").on_input_output()
 
-    temp_index = SummaryIndex.from_documents(
-        documents, service_context=service_context
-    )
-    query_engine = temp_index.as_query_engine(response_mode="tree_summarize")
-    terms_definitions = str(query_engine.query(term_extract_str))
-    terms_definitions = [
-        x
-        for x in terms_definitions.split("\n")
-        if x and "Term:" in x and "Definition:" in x
-    ]
-    # parse the text into a dict
-    terms_to_definition = {
-        x.split("Definition:")[0]
-        .split("Term:")[-1]
-        .strip(): x.split("Definition:")[-1]
-        .strip()
-        for x in terms_definitions
-    }
-    return terms_to_definition
+    # Question/statement relevance between question and each context chunk.
+    f_qs_relevance = Feedback(llm_provider.qs_relevance, name = "Context Relevance").on_input().on(
+        TruLlama.select_source_nodes().node.text
+    ).aggregate(np.mean)
 
+    # groundedness of output on the context
+    groundedness = feedback.Groundedness(
+                    # summarize_provider=azopenai, 
+                    groundedness_provider=llm_provider)
+    f_groundedness = Feedback(groundedness.groundedness_measure, name = "Groundedness").on(TruLlama.select_source_nodes().node.text).on_output()
+    
+    feedbacks=[f_groundedness, f_qa_relevance, f_qs_relevance]
+    
+    return feedbacks
 
-DEFAULT_TERM_STR = (
-    "Make a list of terms and definitions that are defined in the context, "
-    "with one pair on each line. "
-    "If a term is missing it's definition, use your best judgment. "
-    "Write each line as as follows:\nTerm: <term> Definition: <definition>"
-)
+class RAG():
+    llm_list = ['llama2', 'gpt-35-turbo']
+    embedding_list = ['text-embedding-ada-002', 'sentence-transformers/all-mpnet-base-v2', "BAAI/bge-small-en-v1.5"]
 
-st.title("🦙 Llama Index Term Extractor 🦙")
+    def __init__(self, llm_name, embedding_model_name, documents, feedbacks):
 
-setup_tab, upload_tab = st.tabs(["Setup", "Upload/Extract Terms"])
+        self.feedbacks = feedbacks
+        self.llm_name = llm_name
+        self.embedding_model_name = embedding_model_name
+           
+        if llm_name not in self.llm_list:
+            raise modelException(llm_name, self.llm_list)
+        if embedding_model_name not in self.embedding_list:
+            raise modelException(embedding_model_name, self.embedding_list)        
 
-with setup_tab:
-    st.subheader("LLM Setup")
-    api_key = st.text_input("Enter your OpenAI API key here", type="password")
-    llm_name = st.selectbox(
-        "Which LLM?", ["text-davinci-003", "gpt-3.5-turbo", "gpt-4"]
-    )
-    model_temperature = st.slider(
-        "LLM Temperature", min_value=0.0, max_value=1.0, step=0.1
-    )
-    term_extract_str = st.text_area(
-        "The query to extract terms and definitions with.",
-        value=DEFAULT_TERM_STR,
-    )
+        ## Getting the LLM for prediction
+        if llm_name in ['gpt-35-turbo']:
+            self.llm = AzureOpenAI(
+                    # model= os.environ['OPENAI_MODEL_NAME'],
+                    model = llm_name,
+                    deployment_name= os.environ['OPENAI_DEPLOYMENT_NAME'],
+                    api_key=os.environ['OPENAI_API_KEY'],
+                    azure_endpoint=os.environ['OPENAI_DEPLOYMENT_ENDPOINT'],
+                    api_version=os.environ['OPENAI_DEPLOYMENT_VERSION'],
+                )
+        elif llm_name == 'llama2':
+            self.llm = Ollama(model="llama2")
+        
+        ## Gettting the embedding model
+        if embedding_model_name == 'text-embedding-ada-002':
+            self.embedding_model = AzureOpenAIEmbedding(
+                    # model=os.environ['OPENAI_EMBEDDING_MODEL_NAME'],
+                    model=embedding_model_name,
+                    deployment_name=os.environ['OPENAI_EMBEDDING_DEPLOYMENT_NAME'],
+                    api_key=os.environ['OPENAI_API_KEY'],
+                    azure_endpoint=os.environ['OPENAI_DEPLOYMENT_ENDPOINT'],
+                    api_version=os.environ['OPENAI_DEPLOYMENT_VERSION'],
+                )
+        elif embedding_model_name in ['sentence-transformers/all-mpnet-base-v2', "BAAI/bge-small-en-v1.5"]:
+            self.embedding_model = HuggingFaceEmbeddings(
+                    model_name=embedding_model_name)
+        
+        #Set service context, documents and Index
+        self.service_context = ServiceContext.from_defaults(embed_model=self.embedding_model, llm = self.llm)
 
-with upload_tab:
-    st.subheader("Extract and Query Definitions")
-    document_text = st.text_area("Or enter raw text")
-    if st.button("Extract Terms and Definitions") and document_text:
-        with st.spinner("Extracting..."):
-            extracted_terms = extract_terms(
-                [Document(text=document_text)],
-                term_extract_str,
-                llm_name,
-                model_temperature,
-                api_key,
-            )
-        st.write(extracted_terms)
+        set_global_service_context(self.service_context)
 
+        # check if storage already exists (under the specific embedding model)
+        if not os.path.exists("./storage/" + embedding_model):
+            # load the documents and create the index
+            self.documents = documents
+            self.index = VectorStoreIndex.from_documents(self.documents, service_context=self.service_context)
+            # store it for later
+            self.index.storage_context.persist("./storage/" + embedding_model)
+            self.storage_context = None
+        else:
+            # load the existing index from the specific embedding model
+            self.storage_context = StorageContext.from_defaults(persist_dir="./storage/" + embedding_model)
+            self.index = load_index_from_storage(self.storage_context)
+
+        # self.documents = SimpleDirectoryReader(
+        #             input_files=[r"/Users/jeana/Retrieval-Augmented-Generation/LlamaIndex/paul_graham_essay.txt"] #or just indicate the fullpath of the folder containing the data
+        #                         ).load_data()
+        # self.index = VectorStoreIndex.from_documents(self.documents, service_context=self.service_context)
+
+    def query(self, query: str) -> str:
+        ### INSTRUMENT CHAIN FOR LOGGING WITH TRULENS
+
+        query_engine = self.index.as_query_engine()
+
+        tru_query_engine_recorder = TruLlama(query_engine,
+                app_id= "RAG_" + self.llm_name + '_' + self.embedding_model_name,
+                feedbacks=self.feedbacks)
+
+        with tru_query_engine_recorder as recorder:
+            answer = query_engine.query(query)
+            print(answer.get_formatted_sources())
+            print("query was:", query)
+            print("answer was:", answer)
+
+def show_metrics(tru):
+    records, feedback = tru.get_records_and_feedback(app_ids=[])
+    pd.set_option("display.max_colwidth", None)
+    return records[['app_id','ts', "input", "output", 'latency', 'total_cost'] + feedback]
+
+def show_leaderboard(tru):
+    return tru.get_leaderboard(app_ids=[])
