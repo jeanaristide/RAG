bd8ee9f0c941acae101b27ddc2a5d0619f918ac0
jeanaristide <jeanaristide34@gmail.com>
2023-12-29 03:04:36 +1100

    modified:   LlamaIndex/RAG_TruLens_eval.ipynb 	modified:   app.py 	new file:   arxiv_storage/sentence-transformers/all-mpnet-base-v2/default__vector_store.json 	new file:   arxiv_storage/sentence-transformers/all-mpnet-base-v2/docstore.json 	new file:   arxiv_storage/sentence-transformers/all-mpnet-base-v2/graph_store.json 	new file:   arxiv_storage/sentence-transformers/all-mpnet-base-v2/image__vector_store.json 	new file:   arxiv_storage/sentence-transformers/all-mpnet-base-v2/index_store.json 	modified:   default.sqlite

--- app.py
+++ app.py
@@ -19,7 +19,20 @@ litellm.set_verbose=False
 tru = Tru()
 from llama_index import download_loader
 import hashlib
-import arxiv
+import shutil
+
+def delete_folder(folder_path):
+    """
+    Delete a folder and its contents if it exists.
+
+    Parameters:
+    - folder_path (str): The full path to the folder to be deleted.
+    """
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        # print(f"Folder '{folder_path}' successfully deleted.")
+    else:
+        pass
 
 class modelException(Exception):
     def __init__(self, invalid_value, allowed_values):
@@ -34,12 +47,13 @@ def feedbacks(llm_name):
     if llm_name == 'azureoai':
         # Initialize AzureOpenAI-based feedback function collection class:
         llm_provider = feedback.AzureOpenAI(
-                                        deployment_name=os.environ['OPENAI_DEPLOYMENT_NAME'],
-                                        api_key = os.environ['OPENAI_API_KEY'],
-                                        api_version=os.environ['OPENAI_DEPLOYMENT_VERSION'],
-                                        azure_endpoint=os.environ['OPENAI_DEPLOYMENT_ENDPOINT'],
+                                        deployment_name=OPENAI_DEPLOYMENT_NAME,
+                                        api_key = OPENAI_API_KEY,
+                                        api_version=OPENAI_DEPLOYMENT_VERSION,
+                                        azure_endpoint=OPENAI_DEPLOYMENT_ENDPOINT,
                                         # model = os.environ['OPENAI_MODEL_NAME']
                                         )
+
     elif llm_name == 'llama2':
         llm_provider = LiteLLM(model_engine="ollama/llama2", api_base='http://localhost:11434')
 
@@ -81,11 +95,12 @@ class RAG():
             self.llm = AzureOpenAI(
                     # model= os.environ['OPENAI_MODEL_NAME'],
                     model = llm_name,
-                    deployment_name= os.environ['OPENAI_DEPLOYMENT_NAME'],
-                    api_key=os.environ['OPENAI_API_KEY'],
-                    azure_endpoint=os.environ['OPENAI_DEPLOYMENT_ENDPOINT'],
-                    api_version=os.environ['OPENAI_DEPLOYMENT_VERSION'],
+                    deployment_name= OPENAI_DEPLOYMENT_NAME,
+                    api_key= OPENAI_API_KEY,
+                    azure_endpoint= OPENAI_DEPLOYMENT_ENDPOINT,
+                    api_version= OPENAI_DEPLOYMENT_VERSION,
                 )
+            
         elif llm_name == 'llama2':
             self.llm = Ollama(model="llama2")
         
@@ -94,10 +109,10 @@ class RAG():
             self.embedding_model = AzureOpenAIEmbedding(
                     # model=os.environ['OPENAI_EMBEDDING_MODEL_NAME'],
                     model=embedding_model_name,
-                    deployment_name=os.environ['OPENAI_EMBEDDING_DEPLOYMENT_NAME'],
-                    api_key=os.environ['OPENAI_API_KEY'],
-                    azure_endpoint=os.environ['OPENAI_DEPLOYMENT_ENDPOINT'],
-                    api_version=os.environ['OPENAI_DEPLOYMENT_VERSION'],
+                    deployment_name=OPENAI_EMBEDDING_DEPLOYMENT_NAME,
+                    api_key=OPENAI_API_KEY,
+                    azure_endpoint=OPENAI_DEPLOYMENT_ENDPOINT,
+                    api_version=OPENAI_DEPLOYMENT_VERSION,
                 )
         elif embedding_model_name in ['sentence-transformers/all-mpnet-base-v2', "BAAI/bge-small-en-v1.5"]:
             self.embedding_model = HuggingFaceEmbeddings(
@@ -109,16 +124,16 @@ class RAG():
         set_global_service_context(self.service_context)
 
         # check if storage already exists (under the specific embedding model)
-        if not os.path.exists("./storage/" + embedding_model):
+        if not os.path.exists("./arxiv_storage/" + embedding_model_name):
             # load the documents and create the index
             self.documents = documents
             self.index = VectorStoreIndex.from_documents(self.documents, service_context=self.service_context)
             # store it for later
-            self.index.storage_context.persist("./storage/" + embedding_model)
+            self.index.storage_context.persist("./arxiv_storage/" + embedding_model_name)
             self.storage_context = None
         else:
             # load the existing index from the specific embedding model
-            self.storage_context = StorageContext.from_defaults(persist_dir="./storage/" + embedding_model)
+            self.storage_context = StorageContext.from_defaults(persist_dir="./arxiv_storage/" + embedding_model_name)
             self.index = load_index_from_storage(self.storage_context)
 
         # self.documents = SimpleDirectoryReader(
@@ -126,7 +141,7 @@ class RAG():
         #                         ).load_data()
         # self.index = VectorStoreIndex.from_documents(self.documents, service_context=self.service_context)
 
-    def query(self, query: str) -> str:
+    def query(self, query: str):
         ### INSTRUMENT CHAIN FOR LOGGING WITH TRULENS
 
         query_engine = self.index.as_query_engine()
@@ -137,14 +152,17 @@ class RAG():
 
         with tru_query_engine_recorder as recorder:
             answer = query_engine.query(query)
-            print(answer.get_formatted_sources())
-            print("query was:", query)
-            print("answer was:", answer)
+            retrieved_sources = answer.get_formatted_sources()
+            # print(answer.get_formatted_sources())
+            # print("query was:", query)
+            # print("answer was:", answer)
+
+        return answer, retrieved_sources
 
 def show_metrics(tru):
     records, feedback = tru.get_records_and_feedback(app_ids=[])
     pd.set_option("display.max_colwidth", None)
-    return records[['app_id','ts', "input", "output", 'latency', 'total_cost'] + feedback]
+    return records[['app_id','ts', "input", "output", 'latency', 'total_cost'] + feedback].set_index(['ts'])
 
 def show_leaderboard(tru):
     return tru.get_leaderboard(app_ids=[])
@@ -171,34 +189,52 @@ def list_files(directory):
 
 ############################################################################################################
 
+st.set_page_config(
+    layout="wide",
+)
+
 st.title("🦙 Retrieval Augmented Generation based Q&A over Arxiv PDFs 🦙")
 
-setup_tab, document_tab, upload_questions_tab = st.tabs(["Setup", "Select PDFs", "Upload Questions"])
+setup_tab, document_tab, ask_tab, metrics_tab = st.tabs(["Setup", "Select PDFs", "Ask Questions", "Evaluation Metrics"])
 
 with setup_tab:
     st.subheader("LLM Predictor Setup")
     
-    llm_name = st.selectbox(
-        label = "Which LLM?", options = ['llama2', 'gpt-35-turbo'], key = 'llm_predictor', index = None
-    )
-    if llm_name == 'gpt-35-turbo':
-
-        OPENAI_API_KEY = st.text_input("Enter your Azure OpenAI API key here")
-        OPENAI_DEPLOYMENT_ENDPOINT = st.text_input("Enter your Azure OpenAI Deployment Endpoint here")
-        OPENAI_DEPLOYMENT_NAME = st.text_input("Enter your Azure OpenAI LLM Deployment Name here")
-        OPENAI_DEPLOYMENT_VERSION = st.text_input("Enter your Azure OpenAI Deployment Version here")
-
-        model_temperature = st.slider(
-            "LLM Temperature", min_value=0.0, max_value=1.0,
-            step=0.1, disabled= True if st.session_state.llm_predictor == 'llama2' else False
+    with st.form('Select Desired LLM, Embedding Model and Evaluator LLM'):
+        llm_name = st.selectbox(
+            label = "Which LLM?", options = ['llama2', 'gpt-35-turbo'], key = 'llm_predictor', index = None
         )
-    embedding_model_name = st.selectbox(
-        label = "Which Embedding Model?", options = ['text-embedding-ada-002', 'sentence-transformers/all-mpnet-base-v2', "BAAI/bge-small-en-v1.5"]
-            , key = 'embedding_model', index = None
-    )
 
-    if embedding_model_name == 'text-embedding-ada-002':
-        OPENAI_EMBEDDING_DEPLOYMENT_NAME = st.text_input("Enter your Azure OpenAI Embedding Deployment Name here")
+        embedding_model_name = st.selectbox(
+            label = "Which Embedding Model?", options = ['text-embedding-ada-002', 'sentence-transformers/all-mpnet-base-v2', "BAAI/bge-small-en-v1.5"]
+                , key = 'embedding_model', index = None
+        )
+
+        evaluator_llm_option = st.radio(
+                        "Select LLM to evaluate the RAG App",
+                        ["azureoai", "llama2"],
+                        index=None,
+                    )
+
+        submitted_models = st.form_submit_button("Confirm")
+
+        if submitted_models:
+            
+            if llm_name == 'gpt-35-turbo':
+
+                OPENAI_API_KEY = st.text_input("Enter your Azure OpenAI API key here")
+                OPENAI_DEPLOYMENT_ENDPOINT = st.text_input("Enter your Azure OpenAI Deployment Endpoint here")
+                OPENAI_DEPLOYMENT_NAME = st.text_input("Enter your Azure OpenAI LLM Deployment Name here")
+                OPENAI_DEPLOYMENT_VERSION = st.text_input("Enter your Azure OpenAI Deployment Version here")
+
+                model_temperature = st.slider(
+                    "LLM Temperature", min_value=0.0, max_value=1.0,
+                    step=0.1, disabled= True if st.session_state.llm_predictor == 'llama2' else False
+                )
+
+            if embedding_model_name == 'text-embedding-ada-002':
+                OPENAI_EMBEDDING_DEPLOYMENT_NAME = st.text_input("Enter your Azure OpenAI Embedding Deployment Name here")
+            
 
 with document_tab:
     st.subheader("Select Arxiv PDF(s)")
@@ -209,6 +245,8 @@ with document_tab:
         submitted = st.form_submit_button("Submit")
 
         if submitted:
+            delete_folder('./arxiv_storage') ## reset indexed documents if any
+
             directory_path = '.papers'
 
             from llama_index import download_loader
@@ -216,6 +254,40 @@ with document_tab:
             ArxivReader = download_loader("ArxivReader")
 
             loader = ArxivReader()
-            documents = loader.load_data(search_query='id:2311.12399', max_results=max_results)
-            st.write(f"Loaded {len(documents)} Documents")
+            st.session_state.documents = loader.load_data(search_query='id:2311.12399', max_results=max_results)
+            st.write(f"Loaded {len(st.session_state.documents)} Documents")
+
+with ask_tab:
+    st.subheader("Ask Questions over your Arxiv PDFs")
+
+    with st.form('Q&A over Arxiv Docs'):
+        query = st.text_input("Enter your questions here")
+        submitted_query = st.form_submit_button("Submit")
+
+        if submitted_query:
+
+            feedbacks = feedbacks(evaluator_llm_option)
+
+            rag = RAG(llm_name, embedding_model_name, st.session_state.documents, feedbacks)
+
+            answer, retrieved_sources = rag.query(query)
+
+            st.write(f'Answer: {answer}')
+            st.write(f'Source: {retrieved_sources}')
+
+with metrics_tab    :
+    st.subheader("Evaluation Metrics")
+
+    metrics = show_metrics(tru)
+    leaderboard = tru.get_leaderboard(app_ids=[])
+
+    st.write('Leaderboard')
+    st.write(leaderboard)
+    st.write('Metrics')
+    st.write(metrics)
+
+
+
+
+
         
